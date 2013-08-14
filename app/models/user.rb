412c6a157ea0d2b2ac147b26fa8b7af5b8086784
nma <cloakedcyril@gmail.com>
2013-08-14 19:19:01 -0400

    remove attr_accessor which makes fields virtual-only

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase } 
-  #attr_accessor :email, :name
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :name,  :presence => true,
@@ -8,4 +7,5 @@ class User < ActiveRecord::Base
   validates :email, :presence => true,
                     :format   => { :with => VALID_EMAIL_REGEX },
                     :uniqueness => { :case_sensitive => false }
+  #has_secure_password
 end
