ecbefcb53d63d91f856a7bf663403f7ac8fe2fe0
nma <cloakedcyril@gmail.com>
2013-08-15 03:22:25 -0400

    working encrypted password auth with tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,22 @@ require 'spec_helper'
 
 describe User do
   before(:each) do
-    @attr = { :name => "Example User", :email => "user@example.com" }
+    @attr = { :name => "Example User", :email => "user@example.com", 
+      :password => "fake123", :password_confirmation => "fake123"}
+    @user = User.new(@attr)
   end
-  
+
+  subject{@user}
+
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
+
+  it { should be_valid }
+
   it "should create a new instance given valid attributes" do
     User.create!(@attr)
   end
@@ -32,9 +45,9 @@ describe User do
       valid_email_user.should be_valid
     end
   end
-  
+
   it "should reject invalid email addresses" do
-    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar..com]
     addresses.each do |address|
       invalid_email_user = User.new(@attr.merge(:email => address))
       invalid_email_user.should_not be_valid
@@ -56,5 +69,51 @@ describe User do
     user_with_duplicate_email.should_not be_valid
   end
 
+  # new style of tests, technical debt, change to one preferered type (above, below) later on
+  describe "email with mixed case" do
+    let(:mixed_email) { "AusR@EmAil.coM" }
 
+    it "should save email as lower case" do
+      @user.email = mixed_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_email.downcase
+    end
+  end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       password: " ", password_confirmation: " ")
+    end
+    it { should_not be_valid }
+  end
+
+  describe "when password is mismatched" do
+    before do
+      @user.password = "mismatch"
+    end
+    it { should_not be_valid }
+  end
+
+  describe "with a password that's too short" do
+    before { @user.password = @user.password_confirmation = "a" * 5 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) {User.find_by(:email => @user.email)}
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password)}
+    end
+
+    describe "with invalid password" do
+      let(:user_with_invalid_password) { found_user.authenticate("invalid") }
+
+      # it and specify are synonyms, just another way when it sounds awkward in the test context 
+      it { should_not eq user_with_invalid_password }
+      specify { expect(user_with_invalid_password).to be_false }
+    end
+  end
 end
